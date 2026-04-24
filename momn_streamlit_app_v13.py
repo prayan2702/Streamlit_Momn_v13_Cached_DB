4013f9a63d88993825600ce72ea258ef0705dc72
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2026-04-24 17:06:13 +0530

    Update momn_streamlit_app_v13.py

--- momn_streamlit_app_v13.py
+++ momn_streamlit_app_v13.py
@@ -1966,7 +1966,7 @@ with _tab_screener:
                 st.markdown("""
                 <div class="nse-link-box">
                   <div>📥</div>
-                  <div color:#28282B;>
+                  <div>
                     <b>NSE — Securities Available for Trading</b>
                     <div class="hint">
                       GitHub cache se auto-load hoga &nbsp;|&nbsp;
